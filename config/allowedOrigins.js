435cb6195c18921eec6294395e3270a89d979bfb
shamroz73 <shamrozwarraich@gmail.com>
2022-04-02 02:40:32 +0200

    cors issue fixing

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 export const allowedOrigins = [
   "https://www.yoursite.com",
-  "https://advance-mern-auth-shamroz73.vercel.app/",
+  "https://advance-mern-auth.vercel.app",
   "http://127.0.0.1:5500",
   "http://localhost:3000",
   "http://localhost:3500",
